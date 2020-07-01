048183cd617140a7c1390c4fd073661f91d55c7b
Kha Nguyen <ykha@uw.edu>
2020-07-01 15:44:22 -0700

    Adds dynamic breadcrumbs on route change

--- src/components/breadcrumbs/Breadcrumbs.tsx
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+import _ from 'lodash';
 import './Breadcrumbs.css';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
-import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 
-const AppBreadcrumb = () => (
-  <Breadcrumb className="my-breadcrumb">
-    <Breadcrumb.Item>
-      <NavLink
-        to="/hello"
-        activeStyle={{
-          textDecoration: 'none',
-          color: 'black',
-        }}
-      >
-        Overview
-      </NavLink>
-    </Breadcrumb.Item>
-    <Breadcrumb.Item active>Details</Breadcrumb.Item>
-  </Breadcrumb>
-);
+class AppBreadcrumb extends Component<any> {
+  render() {
+    const { goTo, showBreadcrumbs, params } = this.props;
+    console.log('inside render', showBreadcrumbs, params);
 
-export default AppBreadcrumb;
+    return (
+      showBreadcrumbs && (
+        <Breadcrumb className="my-breadcrumb">
+          {_.map(params, function (value, idx) {
+            const isLastElement = idx === params.length - 1;
+            return (
+              <Breadcrumb.Item
+                active={isLastElement}
+                onClick={() => {
+                  if (!isLastElement) {
+                    goTo('/' + value);
+                  }
+                }}
+                key={value}
+              >
+                {value}
+              </Breadcrumb.Item>
+            );
+          })}
+        </Breadcrumb>
+      )
+    );
+  }
+}
+
+const mapStateToProps = (state, props) => {
+  const params = _.omitBy(props.match.params, _.isUndefined);
+  console.log('inside app breadcrumb', params);
+  return {
+    showBreadcrumbs: _.size(params) > 1,
+    params: _.values(params),
+    goTo: props.history.push,
+  };
+};
+
+export default withRouter(connect(mapStateToProps)(AppBreadcrumb));
