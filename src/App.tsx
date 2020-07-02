90883382481031a4968f1d612b234d111121159c
Duc Vo <pianote@gmail.com>
2020-07-01 22:41:47 -0700

    remove uneccesary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import AppRoot from './components/root/AppRoot';
-import NotFound from './components/NotFound/NotFound';
-import { Provider } from 'react-redux';
-import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
-import store from './store';
+
 
 const App = (props) => {
   const { navId } = props
