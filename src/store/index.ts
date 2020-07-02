cd8b61180a64bee3e64cbaa1bcaf9c7fce9a0d9d
Duc Vo <pianote@gmail.com>
2020-07-01 21:39:20 -0700

    add redux-thunk for async code

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,15 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import globalModals from '../components/global-modals/login/login-modal.reducers';
 import auth from '../apis/auth/auth.reducers';
+import reducerFromRootFolder from '../components/root/reducer/reducer';
 
 const appReducers = combineReducers({
+  nav: reducerFromRootFolder,
   auth,
   globalModals,
 });
 
-const store = createStore(appReducers, applyMiddleware(logger));
+const store = createStore(appReducers, applyMiddleware(logger, thunk));
 export default store;
