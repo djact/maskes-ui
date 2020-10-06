711c8c4b262402883405e10138ebba57fd771be9
Zack Seuberling <seuberling@gmail.com>
2020-10-05 22:03:25 -0700

    feat: use index file to export related containers

---
+++ src/containers/Volunteer/index.js
@@ -0,0 +1,13 @@
+import VolunteerList from './VolunteerList/VolunteerList';
+import VolunteerDetail from './VolunteerDetail/VolunteerDetail';
+import MyVolunteerList from './MyVolunteer/MyVolunteerList';
+import MyVolunteerDetail from './MyVolunteer/MyVolunteerDetail';
+import SignUp from './SignUp/SignUp';
+
+export {
+  VolunteerList,
+  VolunteerDetail,
+  MyVolunteerList,
+  MyVolunteerDetail,
+  SignUp,
+};
