252cbb49c35d396ab1b5a24569c1ec9d40fc3665
Duc Vo <pianote@gmail.com>
2020-12-30 16:08:19 -0800

    fix footer link

--- src/hoc/Layout/Footer/Footer.js
+++ src/hoc/Layout/Footer/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Segment, List, Grid, Header, Icon } from 'semantic-ui-react';
-import { Form, Button } from 'react-bootstrap';
+// import { Form, Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './Footer.css';
 
-const Footer = (props) => (
+const Footer = () => (
 	<footer id="footer">
 		<Segment
 			className="waveFooter"
@@ -16,24 +18,50 @@ const Footer = (props) => (
 				<Grid divided inverted stackable>
 					<Grid.Row>
 						<Grid.Column width={4}>
-							<Header inverted as="h4" content="About" />
+							<Header inverted as="h4" content="Pages" />
 							<List link inverted>
-								<List.Item as="a">Contact Us</List.Item>
-								<List.Item as="a">Community Events</List.Item>
-								<List.Item as="a">Mutual Aid Pod</List.Item>
+								<ListItem path="/">Home</ListItem>
+								<ListItem path="/get-help">Get Help</ListItem>
+								<ListItem path="/get-involved">Get Involved</ListItem>
 							</List>
 						</Grid.Column>
 						<Grid.Column width={4}>
-							<Header inverted as="h4" content="Services" />
+							<Header inverted as="h4" content="Links" />
 							<List link inverted>
-								<List.Item as="a">Request Support</List.Item>
-								<List.Item as="a">Offer Support</List.Item>
-								<List.Item as="a">How To Get Help</List.Item>
-								<List.Item as="a">How To Get Involved</List.Item>
+								<ListItem path="/faq#requestingSupport">
+									How To Get Help
+								</ListItem>
+								<ListItem path="/faq#offeringSupport">
+									How To Get Involved
+								</ListItem>
+								<List.Item
+									as="a"
+									onClick={() =>
+										window.open(
+											'https://gdoc.pub/doc/e/2PACX-1vRMxV09kdojzMdyOfapJUOB6Ko2_1iAfIm8ELeIgma21wIt5HoTqP1QXadF01eZc0ySrPW6VtU_veyp',
+											'_blank'
+										)
+									}
+								>
+									Mutual Aid Toolkit
+								</List.Item>
+
+								<List.Item
+									as="a"
+									onClick={() =>
+										window.open(
+											'https://docs.google.com/forms/d/e/1FAIpQLScc_Ok6nxhAOLghITWPhLKGh8uRHEDKexxVerj5L-9BoJo-xA/viewform',
+											'_blank'
+										)
+									}
+								>
+									Mutual Aid Pod
+								</List.Item>
+								<ListItem path="/faq#contactUs">Contact Us</ListItem>
 							</List>
 						</Grid.Column>
 						<Grid.Column width={5}>
-							<Form>
+							{/* <Form>
 								<Form.Control
 									className="subscribe-email"
 									type="email"
@@ -43,15 +71,14 @@ const Footer = (props) => (
 									Subscribe
 								</Button>
 							</Form>
-							<br />
-
+							<br /> */}
 							<Icon
 								name="facebook"
 								className="fb-icon"
 								onClick={() =>
 									window.open('https://www.facebook.com/SKCmutualaid', '_blank')
 								}
-							/>
+							/>{' '}
 							<Icon
 								name="instagram"
 								className="ig-icon"
@@ -62,7 +89,21 @@ const Footer = (props) => (
 									)
 								}
 							/>
-							<Icon name="patreon" className="pt-icon" />
+							<Icon
+								name="patreon"
+								className="pt-icon"
+								onClick={() =>
+									window.open('https://www.patreon.com/skcemutualaid', '_blank')
+								}
+							/>
+							<br />
+							<br />
+							<p className="quote-ma">
+								“Mutual aid is “cooperation for the sake of the common good.”
+								It’s getting people to come together to meet each other’s needs,
+								recognizing that as humans, our survival is dependent on one
+								another.“ - Mariame Kaba
+							</p>
 						</Grid.Column>
 					</Grid.Row>
 					<Grid.Row
@@ -72,12 +113,28 @@ const Footer = (props) => (
 							justifyContent: 'center'
 						}}
 					>
-						<p>Copyright &copy; 2020 SKCEMA</p>
+						{/* <p>Copyright &copy; 2020 SKCEMA</p> */}
 					</Grid.Row>
 				</Grid>
 			</Container>
 		</Segment>
 	</footer>
 );
+
+const ListItem = (props) => {
+	const { path, children } = props;
+	return (
+		<List.Item>
+			<Link className="list-item" to={path}>
+				{children}
+			</Link>
+		</List.Item>
+	);
+};
+
+ListItem.propTypes = {
+	path: PropTypes.string,
+	children: PropTypes.string
+};
 
 export default Footer;
