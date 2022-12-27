ec10679ef37284b1a63d0940bea9da4e45219138
Duc Vo <dvo4@seattleu.edu>
2022-12-26 19:29:17 -0800

    configured amplify authentication

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    auth: {
+        maskesuia5d1df9e: {
+            IdentityPoolId: 'string'
+            IdentityPoolName: 'string'
+            UserPoolId: 'string'
+            UserPoolArn: 'string'
+            UserPoolName: 'string'
+            AppClientIDWeb: 'string'
+            AppClientID: 'string'
+        }
+    }
+}
