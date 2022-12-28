05b16aba2d4ede4244138ded92239f626c1343b5
Duc Vo <dvo4@seattleu.edu>
2022-12-27 17:59:42 -0800

    add comment for _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,9 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 
+/**
+ * Next.js _document.tsx is used to customize the document's HTML and body tags
+ * https://nextjs.org/docs/advanced-features/custom-document
+ */
 const Document = () => {
     return (
         <Html lang="en">
