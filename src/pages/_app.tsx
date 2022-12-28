cbf363807b420457a9228b2378e5476493d9795f
Duc Vo <dvo4@seattleu.edu>
2022-12-27 17:59:22 -0800

    add condition for protected component using auth property in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,28 @@
-import { withAuthenticator } from '@aws-amplify/ui-react'
+import RequireAuth from '@components/RequireAuth'
 import Providers from '@providers'
 import '@styles/globals.css'
 import '@utils/initialize'
 import type { AppProps } from 'next/app'
 
+/**
+ * Next.js _app.tsx is used to initialize pages.
+ * The pages are wrapped with the Providers
+ * Protected page components are wrapped with the RequireAuth
+ * Set auth attribute to true on the page component to protect it
+ * @param props extended AppProps | see @types/next.d.ts
+ */
 const App = (props: AppProps) => {
     const { Component, pageProps } = props
+
     return (
         <Providers>
-            <Component {...pageProps} />
+            {(Component.auth && (
+                <RequireAuth>
+                    <Component {...pageProps} />
+                </RequireAuth>
+            )) || <Component {...pageProps} />}
         </Providers>
     )
 }
 
-export default withAuthenticator(App)
+export default App
