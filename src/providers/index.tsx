8497e0ee9eb02416f454abd4f334e12b1c2d096b
Duc Vo <dvo4@seattleu.edu>
2022-12-27 18:02:17 -0800

    include amplify authenticator provider in global providers component in providers/index.tsx

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,15 +1,22 @@
 import { FC } from 'react'
 import { ThemeProvider } from './ThemeProvider'
+import { Authenticator } from '@aws-amplify/ui-react'
 
 interface ProvidersProps {
     children: React.ReactNode
 }
-
+/**
+ * Providers includes all the providers that are used in the application
+ **/
 const Providers: FC<ProvidersProps> = (props) => {
     const { children } = props
-    return <ThemeProvider>{children}</ThemeProvider>
+    return (
+        <ThemeProvider>
+            <Authenticator.Provider>{children}</Authenticator.Provider>
+        </ThemeProvider>
+    )
 }
 
-export { ThemeProvider }
-
 export default Providers
+
+export { ThemeProvider }
