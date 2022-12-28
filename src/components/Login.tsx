ca69677652984cdad1a086616d283ee5f5079d54
Duc Vo <dvo4@seattleu.edu>
2022-12-27 17:56:33 -0800

    add Login component using amplify Authenticator with redirect to original route after authenticated in Login.tsx

---
+++ src/components/Login.tsx
@@ -0,0 +1,19 @@
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+const Login = () => {
+    const router = useRouter()
+    const { route } = useAuthenticator((context) => [context.route])
+
+    useEffect(() => {
+        if (route === 'authenticated') {
+            const redirect = router.asPath === '/login' ? '/' : router.asPath
+            router.replace(redirect)
+        }
+    }, [route])
+
+    return <Authenticator />
+}
+
+export default Login
