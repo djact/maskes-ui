b27892b26fe8fcfbb76f47f5cf4ed3a451b7ede9
Duc Vo <dvo4@seattleu.edu>
2022-12-27 20:02:34 -0800

    fix indent for amplify auto-generated file

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,13 @@
 export type AmplifyDependentResourcesAttributes = {
-    auth: {
-        maskesuia5d1df9e: {
-            IdentityPoolId: 'string'
-            IdentityPoolName: 'string'
-            UserPoolId: 'string'
-            UserPoolArn: 'string'
-            UserPoolName: 'string'
-            AppClientIDWeb: 'string'
-            AppClientID: 'string'
+    "auth": {
+        "maskesuia5d1df9e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
         }
     }
 }
