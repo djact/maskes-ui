5acb398469fc8d36d4b2de96012d781359df3ff0
Duc Vo <dvo4@seattleu.edu>
2022-12-29 18:59:51 -0800

    include bootstrap cdn stylesheet and js files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,16 @@ import { Head, Html, Main, NextScript } from 'next/document'
 const Document = () => {
     return (
         <Html lang="en">
+            <link
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+                integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+                crossOrigin="anonymous"
+            />
+            <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"></script>
+            <script>var Alert = ReactBootstrap.Alert;</script>
             <Head />
             <body>
                 <Main />
